2f95aa314b19053aee7add3b45a127835d17a82c
GaliFrey <zagoskin2013@gmail.com>
2019-11-17 15:19:41 +0300

    Добавлена папка src для проекта

---
+++ _HomeWork/6-setting-react-app/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <div className="container">
+        <h3>Hello World!</h3>
+      </div>
+    )
+  }
+}
+
+export default App
